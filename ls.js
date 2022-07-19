c17c8a4439320cac69e97562a63587da9c72c725
gfranca11 <87835304+gfranca11@users.noreply.github.com>
2022-07-19 16:30:00 -0300

    new

---
+++ ls.js
@@ -0,0 +1,22 @@
+function Entrar(){
+  let menu =  document.getElementById('f');
+    if (menu.style.display == "none") {
+        menu.style.display = "block";
+    }
+    else(
+        menu.style.display = "none"
+       
+
+    )
+}
+function proposta(){
+    let proposta = document.getElementById('p');
+    if (proposta.style.display == "none") {
+        proposta.style.display = "block";
+        
+    }
+    else{
+        proposta.style.display = "none"
+       
+    }
+}
